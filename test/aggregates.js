a70c76098b858dcbf5b688118cfa601269c6926c
Thomas Bradford <thom@kode4food.it>
2013-10-26 12:12:02 +0200

    Adding more tests for grouping

--- test/aggregates.js
+++ test/aggregates.js
@@ -7,17 +7,17 @@ require('../junqi/extensions');
 exports.aggregates = nodeunit.testCase({
   setUp: function (callback) {
     this.data = [
-      { "firstName": "Bill", "lastName": "Bradley", "age": 30 },
       { "firstName": "Thom", "lastName": "Bradford", "age": 40 },
+      { "firstName": "Bill", "lastName": "Bradley", "age": 30 },
       { "firstName": "Stefano", "lastName": "Rago", "age": 29 },
       { "firstName": "Fred", "lastName": "Wilkinson", "age": 50 },
       { "firstName": "Ted", "lastName": "Williams", "age": 20 },
       { "firstName": "Jed", "lastName": "Clampet", "age": 70 },
-      { "firstName": "Will", "lastName": "Robinson", "age": 45 },
-      { "firstName": "John", "lastName": "Jacob", "age": 54 },
-      { "firstName": "Thom", "lastName": "Smith", "age": 15 },
+      { "firstName": "Will", "lastName": "Williams", "age": 45 },
+      { "firstName": "John", "lastName": "Bradley", "age": 54 },
+      { "firstName": "Thom", "lastName": "Bradford", "age": 15 },
       { "firstName": "Bill", "lastName": "Blake", "age": 90 },
-      { "firstName": "Hank", "lastName": "Williams", "age": 70 },
+      { "firstName": "Hank", "lastName": "Bradley", "age": 70 },
       { "firstName": "John", "lastName": "Cash", "age": 45 },
       { "firstName": "Joe", "lastName": "Strummer", "age": 54 },
       { "firstName": "Ted", "lastName": "Turner", "age": 15 }
@@ -45,7 +45,13 @@ exports.aggregates = nodeunit.testCase({
 
   "Grouping Aggregation Works": function (test) {
     test.equal(objeq(this.data, "group firstName := count")[0], 2,
-      "Aggregated Count is correct");
+      "Single-level grouped count is correct");
+
+    test.equal(objeq(this.data, "group lastName, firstName := count")[0], 2,
+      "Nested grouped count is correct");
+
+    test.equal(objeq(this.data, "group lastName + firstName := count")[0], 2,
+      "Expression-based grouped count is correct");
 
     test.done();
   }
