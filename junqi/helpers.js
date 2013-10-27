47904ac00c0b7bf835fbb1f5f4c2944a4cbfd63d
Thomas Bradford <thom@kode4food.it>
2013-10-27 15:59:51 +0100

    wrapping/unwrapping

--- junqi/helpers.js
+++ junqi/helpers.js
@@ -13,12 +13,25 @@ function createNodeFilter(query, callback) {
   return nodeFilter;
 
   function nodeFilter(err, data) {
+    var args = slice.call(arguments, 1)
+      , wrapped = false;
+
     if ( err ) {
       callback(err, null);
       return;
     }
 
+    // If the payload isn't an array, wrap it
+    if ( !Array.isArray(data) ) {
+      args[0] = [data];
+      wrapped = true;
+    }
+
     var result = query.apply(null, slice.call(arguments, 1));
+    if ( wrapped && result.length <= 1 ) {
+      result = result[0];
+    }
+
     callback(null, result);
   }
 }
